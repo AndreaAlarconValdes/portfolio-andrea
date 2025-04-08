18c8426cb1d83dd37dd08cbcf28d00b0a983f70f
andreaalarconvaldes <andreaalarconvaldes@gmail.com>
2025-04-08 18:41:36 +0100

    Create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="title-container">
+        <h1>Hi, I'm Andrea</h1>
+        <h4>FULL STACK DEVELOPER</h4>
+      </div>
+      <div className="social-media">
+      <i className="fa-solid fa-envelope"></i>
+      <i className="fa-brands fa-linkedin-in"></i>
+      <i className="fa-brands fa-github"></i>
+      <i className="fa-solid fa-file"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
