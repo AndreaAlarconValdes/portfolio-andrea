56ffc0af1349d7a48f8fe245956a50ce2f5a0237
andreaalarconvaldes <andreaalarconvaldes@gmail.com>
2025-04-27 21:34:35 +0100

    resume page update

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,99 +1,78 @@
+import Box from "../components/Box";
 import FilterableList from "../components/FilterableList";
 import "./Resume.css";
+import {
+  education,
+  skills,
+  skillsFilters,
+  languages,
+  experience,
+} from "../constants/constants.ts";
 
 const Resume = () => {
-  const skills = [
-    { icon: "react", name: "react", category: "Frontend" },
-    { icon: "html", name: "html5", category: "Frontend" },
-    { icon: "css", name: "css3", category: "Frontend" },
-    { icon: "tailwind", name: "tailwind css", category: "Frontend" },
-    { icon: "js", name: "javascript", category: "Frontend" },
-    { icon: "ts", name: "typescript", category: "Frontend" },
-    { icon: "nodejs", name: "node.js", category: "Backend" },
-    { icon: "mongodb", name: "mongodb", category: "Backend" },
-    { icon: "mysql", name: "mysql", category: "Backend" },
-    { icon: "postgresql", name: "postgresql", category: "Backend" },
-    { icon: "docker", name: "docker", category: "Backend" },
-    { icon: "git", name: "git", category: "Others" },
-    { icon: "figma", name: "figma", category: "Others" },
-    { icon: "google-ads", name: "google ads", category: "Others" },
-    {
-      icon: "wordpress",
-      name: "wordpress",
-      category: "Others",
-    },
-    { icon: "shopify", name: "shopify", category: "Others" },
-  ];
-
-  const skillsFilters = [
-    { name: "All", icon: "fa-solid fa-globe" },
-    { name: "Frontend", icon: "fa-solid fa-desktop" },
-    { name: "Backend", icon: "fa-solid fa-gear" },
-    { name: "Others", icon: "fa-solid fa-wrench" },
-  ];
-
-  // const education = [
-  //   { program: "Google Ads Certification", school: "Grow with Google" },
-  //   { program: "Software Engineering Bootcamp", school: "Ironhack" },
-  //   { program: "Science Bachelor Degree", school: "IES Bruguers" },
-  // ];
-
-  // const experience = [
-  //   {
-  //     year: "2024 - present",
-  //     position: "Supervisor",
-  //     company: "Jump Juice Bar",
-  //     location: "Dublin, Ireland",
-  //   },
-  //   {
-  //     year: "Jan 2023 - Dec 2023",
-  //     position: "Full Stack Developer",
-  //     company: "Haddock",
-  //     location: "Barcelona, Spain",
-  //   },
-  //   {
-  //     year: "Nov 2020 - Dec 2022",
-  //     position: "Administrative",
-  //     company: "Barcelona Epidemiology Service",
-  //     location: "Barcelona, Spain",
-  //   },
-  // ];
-
-  const languages = [
-    { language: "English", level: "Advanced" },
-    { language: "Spanish", level: "Native" },
-    { language: "Catalan", level: "Native" },
-  ];
-
-  
-  
-
-
   return (
     <div id="resume" className="resume-page">
-      <div className="languages">
-        <div className="languages-title">
-          <i className="fa-solid fa-chevron-left"></i>
-          <h2>Languages</h2>
-        </div>
-        <div className="languages-content">
-          <ul>
-            {languages.map((item) => (
-              <li>
-                <i className="fa-solid fa-check"></i>
-                <h3>{item.language}</h3>
-                <p>{item.level}</p>
-              </li>
+      <div className="left-column">
+        <Box title="Education" className="education-box">
+          <div className="education-container">
+            {education.map((item) => (
+              <div className="education-items">
+                <h3>{item.program}</h3>
+                <p>
+                  {item.school}, {item.year}
+                </p>
+              </div>
             ))}
-          </ul>
-        </div>
+          </div>
+        </Box>
       </div>
-      <FilterableList
-        items={skills}
-        itemsTitle="Skills"
-        filters={skillsFilters}
-        filterTitle="Technical Skills"
-      />
+      <div className="center-column">
+        <div className="languages">
+          <div className="languages-title">
+            <i className="fa-solid fa-chevron-left"></i>
+            <h2>Languages</h2>
+          </div>
+          <div className="languages-content">
+            <ul>
+              {languages.map((item) => (
+                <li>
+                  <i className="fa-solid fa-check"></i>
+                  <h3>{item.language}</h3>
+                  <p>{item.level}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <FilterableList
+          items={skills}
+          itemsTitle="Skills"
+          filters={skillsFilters}
+          filterTitle="Technical Skills"
+        />
+      </div>
+      <div className="right-column">
+        <Box title="Work experience" className="experience-box">
+          <div className="education-container">
+            {experience.map((item) => (
+              <div>
+                <div className="experience-title">
+                  <h3>{item.company}</h3>
+                  <p>
+                    {item.location} | {item.year}
+                  </p>
+                </div>
+                <div className="roll-description">
+                  <h4>{item.position}</h4>
+                  <p>
+               {item.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </Box>
+      </div>
     </div>
   );
 };
